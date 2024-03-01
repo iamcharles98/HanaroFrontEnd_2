777157ecf40c893cb2af5f1b0e938e2d4e0fcf0a
chansoo <chansoo38@gmail.com>
2024-03-01 17:25:43 +0900

    <FEAT> : Layout 컴포넌트 및 기능 구현

---
+++ src/component/Layout.js
@@ -0,0 +1,46 @@
+
+import { Outlet,useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { MyContext } from "./myContext";
+
+function Layout() {
+  const navigate = useNavigate();
+  const context = useContext(MyContext);
+
+  const onSignOut = () => {
+    localStorage.clear();
+    context.isLogin = false;
+    alert("로그아웃 되었습니다.");
+    navigate("/");
+  }
+  return (
+    <>
+        <nav className="navbar navbar-expand-md navbar-dark bg-primary">
+            <div className="container-fluid">
+                <span className="navbar-brand">Hanaro Album</span>
+                <div className="d-flex justify-content-end">
+
+                    <div className="navbar-nav me-auto mb-2 mb-md-0 ">
+                      {
+                        context.isLogin ?
+                        <>
+                        <span style={{"marginRight" : "8px", "alignContent" : "center"}}>3</span>
+                        <span>TEST</span>
+                        </>
+                        :null
+                      }
+                    </div>
+
+                    <div className="d-flex">
+                        <button className="btn btn-success" onClick={onSignOut}>Sign Out</button>
+                    </div>
+                    
+                </div>
+            </div>
+        </nav>
+      <Outlet />
+    </>
+  );
+}
+
+export default Layout;
